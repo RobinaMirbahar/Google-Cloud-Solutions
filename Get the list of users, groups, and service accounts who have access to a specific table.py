9d95b6cbdd155051c4d189a28702a1f88938ffd3
Robina <60986830+RobinaMirbahar@users.noreply.github.com>
2023-03-20 10:18:47 +0500

    Update Get the list of users, groups, and service accounts who have access to a specific table.py

--- Get the list of users, groups, and service accounts who have access to a specific table.py
+++ Get the list of users, groups, and service accounts who have access to a specific table.py
@@ -1,42 +1,15 @@
-The BigQuery API is to get the list of users, groups, and service accounts who have access to a specific table, along with the columns they have access to. 
-
-Example Python code that uses the BigQuery API to get this information:
-
-from google.oauth2 import service_account
 from google.cloud import bigquery
 
-# Set the credentials
-credentials = service_account.Credentials.from_service_account_file(
-    filename='.../key.json',
-    scopes=['https://www.googleapis.com/auth/cloud-platform'])
+# Initialize a BigQuery client
+client = bigquery.Client()
 
-# Set the project ID and dataset ID
-project_id = 'your-project-id'
-dataset_id = 'your-dataset-id'
-
-# Set the table ID
-table_id = 'your-table-id'
-
-# Create the BigQuery client
-client = bigquery.Client(project=project_id, credentials=credentials)
-
-# Get the table reference
-table_ref = client.dataset(dataset_id).table(table_id)
-
-# Get the table metadata
+# Get a reference to the table you want to set access controls for
+table_ref = client.dataset('my_dataset').table('my_table')
 table = client.get_table(table_ref)
 
-# Get the table access control list
-acl = table.acl
-
-# Print the users, groups, and service accounts who have access to the table
-for entry in acl:
-    print(entry.entity_type, entry.entity_id, entry.role)
-    
-    # Get the columns that the user/group/service account has access to
-    for col_acl in entry.get('access', []):
-        print('  ', col_acl.column_name, col_acl.role)
-        
-The provided code leverages the bigquery.Client object to retrieve the metadata associated with a particular table. Subsequently, it fetches the access control list 
-for the table by utilizing the acl property.Afterwards, it iterates through each entry in the access control list and displays the type of entity (such as user, group, or service account),
-the corresponding entity ID (email address or service account ID), and the role assigned to that entity for the table.
+# Set access controls for the table
+policy = bigquery.IAMPolicy()
+policy.viewers.add('user:viewer@example.com')
+policy.owners.add('user:owner@example.com')
+table.iam_policy = policy
+client.update_table(table, ['iam_policy'])
